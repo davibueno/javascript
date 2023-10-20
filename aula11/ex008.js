1557f8b5ac8f99bbe71d42a50b9c260a42add660
Davi Bueno <133988365+davibueno@users.noreply.github.com>
2023-10-20 15:06:09 -0300

    fiz aula de js aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel} KM/H`)
+if( vel > 60){
+    console.log('Você ultrapassou o limite de velociadade, MULTADO!')
+}
+console.log('diriga sempre com cinto de segurança')
