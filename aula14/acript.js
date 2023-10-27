90b3f20cbb09ecee43ed2754624da9e8fed92cb9
Davi Bueno <133988365+davibueno@users.noreply.github.com>
2023-10-27 11:01:28 -0300

    fazendo outro ex de js

---
+++ aula14/acript.js
@@ -0,0 +1,6 @@
+function verificador(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('textano')
+    var res = docmunt.getElementById('res')
+}
