ee6cbaedfa8e8d92c91adbd106add99cbd8d5d4f
Davi Bueno <133988365+davibueno@users.noreply.github.com>
2023-10-30 14:09:58 -0300

    testei  js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,5 +1,10 @@
-var C = 1
+/*var C = 1
 do{
     console.log(`Passo ${C}`)
     C ++
-}while(C <= 6)
+}while(C <= 6)*/
+consoleg.log( 'Vai começar')
+for(var c = 1; c<=10; c++){
+    console.log(c)
+}
+console.log('FIM!')
